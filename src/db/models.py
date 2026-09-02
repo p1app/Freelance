20a3411a23069da40646cef096ac842650aab8e8
p1app <ramazan.kazimagomedov2008@gmail.com>
2026-09-02 19:46:04 +0300

    update User model

--- src/db/models.py
+++ src/db/models.py
@@ -8,15 +8,15 @@ from src.db.enums import RoleEnum
 class User(Base):
     __tablename__ = "users"
 
-    email: Mapped[String] = mapped_column(String(255), unique=True, index=True)
-    username: Mapped[String] = mapped_column(String(100), unique=True, index=True)
+    email: Mapped[String] = mapped_column(String(255), unique=True, index=True, nullable=False)
+    username: Mapped[String] = mapped_column(String(100), unique=True, index=True, nullable=False)
     hashed_password: Mapped[String] = mapped_column(String(255))
     role: Mapped[RoleEnum] = mapped_column(
-        default = RoleEnum.CLIENT
+        default = RoleEnum.CLIENT, nullable=False
     )
-    fullname: Mapped[String] = mapped_column(String(255))
-    bio: Mapped[Text]
-    skills: Mapped[ARRAY | None] = mapped_column(ARRAY(String))
+    fullname: Mapped[String] = mapped_column(String(255), nullable=False)
+    bio: Mapped[Text | None] = mapped_column(nullable=True)
+    skills: Mapped[ARRAY | None] = mapped_column(ARRAY(String), nullable=True)
     raiting: Mapped[Float] = mapped_column(default=0)
     compeleted_projects: Mapped[Integer] = mapped_column(default=0)
     is_active: Mapped[Boolean] = mapped_column(default=True)
